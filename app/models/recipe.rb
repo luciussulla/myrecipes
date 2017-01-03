d9a42bb1ceda939d296eb484468b35876aca2184
jakub <maciek.kowalczyk@yndex.com>
2017-01-03 17:53:06 +0000

    add mamny to many associations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,13 @@
 class Recipe < ActiveRecord::Base
     has_many :likes
     belongs_to :chef
+    
+    has_many :recipe_styles
+    has_many :styles, through: :recipe_styles
+    
+    has_many :recipe_ingredients
+    has_many :ingredients, through: :recipe_ingredients
+    
     validates :chef_id,     presence: true
     validates :name,        presence: true, length: {minimum: 5, maximum: 100}  
     validates :summary,     presence: true, length: {minimum: 5, maximum: 150} 
